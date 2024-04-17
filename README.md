4a706817feb00d6c98cbd4687fdb921d8cc002ff
Gil Forsyth <gil@forsyth.dev>
2024-04-17 11:00:20 -0400

    use iter_markers

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Tricky bit is xfail marks inside parameterized tests:
 If we can state safely (enough) that all parametrized tests of note have IDs (well, they all do, but some would be numbered):
 
 dictionary of 
-nodeid : `pytest.Param(param, values, id="same_as_upstream", marks=theseus_marker)`
+nodeid : `pytest.Param(param, values, id="same_as_upstream", marks=downstream_marker)`
 
 then we mutate the tests on collection and for a given nodeid, if there's a param with a matching id, we replace it.
 
--- conftest.py
+++ conftest.py
@@ -6,16 +6,16 @@ from test_a import xfail_params
 def pytest_runtest_setup(item: pytest.Item) -> None:
     if (patch_param := xfail_params.get(item.nodeid)) is not None:
         new_params = []
-        for i, mark in enumerate(item.own_markers):
+        for mark in item.iter_markers():
             if mark.name == "parametrize":
-                index = i
-        mark = item.own_markers[index]
-        for param in mark.args[1]:
-            if param.id != patch_param.id:
-                new_params.append(param)
-            else:
-                new_params.append(patch_param)
+                for param in mark.args[1]:
+                    if param.id != patch_param.id:
+                        new_params.append(param)
+                    else:
+                        new_params.append(patch_param)
 
-        patch_mark = pytest.Mark(mark.name, (mark.args[0], new_params), mark.kwargs)
-        item.own_markers.pop(index)
-        item.add_marker(pytest.MarkDecorator(patch_mark))
+                patch_mark = pytest.Mark(
+                    mark.name, (mark.args[0], new_params), mark.kwargs
+                )
+                item.own_markers.remove(mark)
+                item.add_marker(pytest.MarkDecorator(patch_mark))
