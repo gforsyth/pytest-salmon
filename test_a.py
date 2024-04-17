147226a17ba1f49ff16eb6caf3272a1db16bda2a
Gil Forsyth <gil@forsyth.dev>
2024-04-17 10:28:20 -0400

    this feels like it _should_ work but it doesn't

--- test_a.py
+++ test_a.py
@@ -1,5 +1,13 @@
 import pytest
 
+xfail = pytest.mark.xfail("my_downstream_thing")
+
+xfail_params = {
+    "test_a.py::test_param_needs_mark[third]": pytest.param(
+        "False, 0", id="third", marks=pytest.mark.xfail("nope")
+    )
+}
+
 from upstream.a_upstream import *
 
 test_that_fails_downstream = pytest.mark.xfail()(test_that_fails_downstream)
