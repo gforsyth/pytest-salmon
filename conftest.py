b9551f0a8cd46600ac2756bf44826f8da36821e8
Gil Forsyth <gil@forsyth.dev>
2024-04-18 09:38:53 -0400

    refactor: expanded ability to specify groups of parameters

--- conftest.py
+++ conftest.py
@@ -1,8 +1,42 @@
+import re
+
 import pytest
 
-from test_a import xfail_params
+from test_a import xfails
+
+TEST_NAME_IDS = re.compile(r"(.*)\[(?:(.*)-)*(.*)\]")
 
 
 def pytest_runtest_setup(item: pytest.Item) -> None:
-    if (patch_marker := xfail_params.get(item.nodeid)) is not None:
+    if (patch_marker := xfails.get(item.originalname)) is not None:
+        # This is the case where we have a parametrized test but we want to fail
+        # ALL of them so the item.name might be
+        # `test_corr_cov[no_cond-covar_pop]` but we need to check if
+        # `test_corr_cov` by itself is a marker key
         item.add_marker(patch_marker)
+    elif (patch_marker := xfails.get(item.name)) is not None:
+        # This is the case where we have specified an exact test to fail, contra
+        # to the previous condition, maybe we explicitly want to fail _only_
+        # `test_corr_cov[no_cond-covar_pop]`.
+        item.add_marker(patch_marker)
+    else:
+        # Finally, we may have specified a parameter to fail and want to mark
+        # any test that it has been attached to via combination with other
+        # parameters.
+        # So for the test `test_corr_cov[no_cond-covar_pop]`, we may have specified
+        # `test_corr_cov[covar_pop]` as a failure case. So we split out the 1+
+        # parameter names and check them all in sequence
+        for single_node_id in _unnest_node_ids(item.name):
+            if (patch_marker := xfails.get(single_node_id)) is not None:
+                item.add_marker(patch_marker)
+
+
+def _unnest_node_ids(itemname: str) -> list[str]:
+    match = re.match(TEST_NAME_IDS, itemname)
+    if match is None:
+        yield itemname
+    else:
+        test_name, *ids = match.groups()
+
+        for _id in filter(None, ids):
+            yield f"{test_name}[{_id}]"
--- test_a.py
+++ test_a.py
@@ -1,13 +1,12 @@
 import pytest
 
+from upstream.a_upstream import *
+
 notimpl = pytest.mark.xfail(reason="not implemented in backend")
 notyet = pytest.mark.xfail(reason="not implemented upstream")
 
-xfail_params = {
-    "test_a.py::test_param_needs_mark[third]": notimpl,
-    "test_a.py::test_param_needs_mark[4]": notyet,
+xfails = {
+    "test_param_needs_mark[third]": notimpl,
+    "test_param_needs_mark[4]": notyet,
+    "test_that_fails_downstream": pytest.mark.xfail(),
 }
-
-from upstream.a_upstream import *
-
-test_that_fails_downstream = pytest.mark.xfail()(test_that_fails_downstream)
